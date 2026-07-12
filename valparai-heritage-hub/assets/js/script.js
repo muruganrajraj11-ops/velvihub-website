3cfaac1a2cc0868727a97c055b378f40ac3f580f
muruganrajraj11-ops <muruganraj.raj11@gmail.com>
2026-07-12 16:04:29 +0530

    Update script.js

--- valparai-heritage-hub/assets/js/script.js
+++ valparai-heritage-hub/assets/js/script.js
@@ -1,1 +1,17 @@
+// Reading Progress Bar
 
+window.addEventListener("scroll", () => {
+
+    const winScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+    const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+    const progress = (winScroll / height) * 100;
+
+    document.getElementById("reading-progress").style.width =
+        progress + "%";
+
+});
