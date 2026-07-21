73aa345bd8be42d2b12aa7809a0a0a96862d62ea
muruganrajraj11-ops <muruganraj.raj11@gmail.com>
2026-07-21 22:03:06 +0530

    Update script.js

--- valparai-heritage-hub/assets/js/script.js
+++ valparai-heritage-hub/assets/js/script.js
@@ -73,3 +73,21 @@ if (menuToggle && navbar) {
     });
 
 }
+// Back To Top
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
